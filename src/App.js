94e9f89b1780ad3fb81f7a9fa0b880df7dc5ba00
Meskerem Zelalem - Partner/Gebeya <Meskerem.Zelalem@partner.safaricom.et>
2023-03-30 16:50:15 +0300

    new commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Maintenance from "./component/society-maintenance/Maintenance"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <Maintenance/>
+    
+    </div>
+  );
+}
+
+export default App;
